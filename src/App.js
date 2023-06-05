642589ab6b0b2d158647772eb8c26f9ade7df585
Azizxoja <https://github.com/AzizxojaMaxmudov487/Example.git>
2023-06-05 22:27:20 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './pricetracker/main.css';
+import Logo from './pricetracker/Logo';
+import Coin from './pricetracker/Coin';
+
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+      <Coin />
+    </div>
+  );
+}
+
+export default App;
